71157e44bbcc38e31befd6d1e0642e46ccaedd67
joergboe <joergboe@de.ibm.com>
2019-07-03 13:16:14 +0200

    Corrections test samples case

--- tests/frameworktests/tests/CollectionStreamsxInet/Samples/TestCase.sh
+++ tests/frameworktests/tests/CollectionStreamsxInet/Samples/TestCase.sh
@@ -5,8 +5,9 @@ setCategory 'quick'
 function testStep {
 	local save="$PWD"
 	cd "$TTRO_streamsxInetServerSamplesPath/$TTRO_variantCase"
+	pwd
 	export SPL_CMD_ARGS=''
-	export STREAMS_INET_TOOLKIT="$TT_toolkitPath"
+	export STREAMS_INETSERVER_TOOLKIT="$TTPR_streamsxInetServerToolkit"
 	echoExecuteAndIntercept2 'success' 'make'
 	cd "$save"
 	return 0
